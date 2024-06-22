97778ca78153418a630b596295bddf5f29e51cd5
Gregory Casamento <greg.casamento@gmail.com>
2024-06-21 20:57:37 -0400

    Initial Commit

---
+++ NSTreeNode_test/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  NSTreeNode_test
+//
+//  Created by Gregory John Casamento on 6/21/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ NSTreeNode_test/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  NSTreeNode_test
+//
+//  Created by Gregory John Casamento on 6/21/24.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
